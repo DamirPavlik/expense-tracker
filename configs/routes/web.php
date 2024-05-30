94e073601b077344811025866cd062b9e5aa6eaa
DamirPavlik <damir.pavlik@gmail.com>
2024-05-31 00:30:53 +0200

    feat: added routes for auth in web.php

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -2,9 +2,15 @@
 
 declare(strict_types = 1);
 
+use App\Controllers\AuthController;
 use App\Controllers\HomeController;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', [HomeController::class, 'index']);
+    $app->get(pattern:'/', callable: [HomeController::class, 'index']);
+
+    $app->get(pattern: '/login', callable: [AuthController::class, 'loginView']);
+    $app->get(pattern: '/register', callable: [AuthController::class, 'registerView']);
+    $app->post(pattern: '/login', callable: [AuthController::class, 'logIn']);
+    $app->post(pattern: '/register', callable: [AuthController::class, 'register']);
 };
