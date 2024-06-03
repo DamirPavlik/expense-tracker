84766593bb930bbeb2c9985d4c874ede7cee34bb
DamirPavlik <damir.pavlik@gmail.com>
2024-06-03 23:42:53 +0200

    feat: added logout route and added middleware for other routes

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -4,13 +4,17 @@ declare(strict_types = 1);
 
 use App\Controllers\AuthController;
 use App\Controllers\HomeController;
+use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 use Slim\App;
 
 return function (App $app) {
-    $app->get(pattern:'/', callable: [HomeController::class, 'index']);
+    $app->get(pattern:'/', callable: [HomeController::class, 'index'])->add(AuthMiddleware::class);
 
-    $app->get(pattern: '/login', callable: [AuthController::class, 'loginView']);
-    $app->get(pattern: '/register', callable: [AuthController::class, 'registerView']);
-    $app->post(pattern: '/login', callable: [AuthController::class, 'logIn']);
-    $app->post(pattern: '/register', callable: [AuthController::class, 'register']);
+    $app->get(pattern: '/login', callable: [AuthController::class, 'loginView'])->add(GuestMiddleware::class);
+    $app->get(pattern: '/register', callable: [AuthController::class, 'registerView'])->add(GuestMiddleware::class);
+    $app->post(pattern: '/login', callable: [AuthController::class, 'logIn'])->add(GuestMiddleware::class);
+    $app->post(pattern: '/register', callable: [AuthController::class, 'register'])->add(GuestMiddleware::class);
+
+    $app->post(pattern: '/logout', callable: [AuthController::class, 'logOut'])->add(AuthMiddleware::class);
 };
