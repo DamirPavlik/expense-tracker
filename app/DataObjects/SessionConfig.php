772caf3d64eb765ee5e741f9a969380844f4a7ce
DamirPavlik <damir.pavlik@gmail.com>
2024-06-09 12:28:38 +0200

    refactor: minor session refactor

--- app/DataObjects/SessionConfig.php
+++ app/DataObjects/SessionConfig.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\DataObjects;
 
 use App\Enum\SameSite;
@@ -7,11 +9,11 @@ use App\Enum\SameSite;
 class SessionConfig
 {
     public function __construct(
-      public readonly string $name,
-      public readonly string $flashName,
-      public readonly bool $secure,
-      public readonly bool $httponly,
-      public readonly SameSite $sameSite,
-
-    ) {}
+        public readonly string $name,
+        public readonly string $flashName,
+        public readonly bool $secure,
+        public readonly bool $httpOnly,
+        public readonly SameSite $sameSite
+    ) {
+    }
 }
--- app/Session.php
+++ app/Session.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App;
 
 use App\Contracts\SessionInterface;
@@ -8,7 +10,6 @@ use App\Exception\SessionException;
 
 class Session implements SessionInterface
 {
-
     public function __construct(private readonly SessionConfig $options)
     {
     }
@@ -20,14 +21,14 @@ class Session implements SessionInterface
         }
 
         if (headers_sent($fileName, $line)) {
-            throw new SessionException('Headers already sent by ' . $fileName . ": ". $line);
+            throw new SessionException('Headers have already sent by ' . $fileName . ':' . $line);
         }
 
         session_set_cookie_params(
             [
                 'secure'   => $this->options->secure,
-                'httponly' => $this->options->httponly,
-                'samesite' => $this->options->sameSite->value
+                'httponly' => $this->options->httpOnly,
+                'samesite' => $this->options->sameSite->value,
             ]
         );
 
@@ -35,8 +36,8 @@ class Session implements SessionInterface
             session_name($this->options->name);
         }
 
-        if (!session_start()) {
-            throw new SessionException("unable to start the session, skill issues");
+        if (! session_start()) {
+            throw new SessionException('Unable to start the session');
         }
     }
 
@@ -47,17 +48,17 @@ class Session implements SessionInterface
 
     public function isActive(): bool
     {
-       return session_status() === PHP_SESSION_ACTIVE;
-    }
-
-    public function has(string $key): bool
-    {
-        return array_key_exists($key, $_SESSION);
+        return session_status() === PHP_SESSION_ACTIVE;
     }
 
     public function get(string $key, mixed $default = null): mixed
     {
         return $this->has($key) ? $_SESSION[$key] : $default;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $_SESSION);
     }
 
     public function regenerate(): bool
