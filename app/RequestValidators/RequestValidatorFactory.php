04f5d41044722d34d5a7c1929d70630688f925fd
DamirPavlik <damir.pavlik@gmail.com>
2024-06-15 01:58:11 +0200

    feat: added RequestValidatorFactory

---
+++ app/RequestValidators/RequestValidatorFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\RequestValidators;
+
+use App\Contracts\RequestValidatorFactoryInterface;
+use App\Contracts\RequestValidatorInterface;
+use http\Exception\RuntimeException;
+use Psr\Container\ContainerInterface;
+
+class RequestValidatorFactory implements RequestValidatorFactoryInterface
+{
+
+    public function __construct(private readonly ContainerInterface $container)
+    {
+    }
+
+    public function make(string $class): RequestValidatorInterface
+    {
+        $validator = $this->container->get($class);
+
+        if ($validator instanceof RequestValidatorInterface) {
+            return $validator;
+        }
+
+        throw new RuntimeException('Failed to instantiate the request validator class "' . $class . '"');
+    }
+}
