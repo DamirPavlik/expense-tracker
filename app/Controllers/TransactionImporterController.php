11cc0529d819e4676b439bb95d5d135e5299c333
DamirPavlik <damir.pavlik@gmail.com>
2024-06-17 10:14:38 +0200

    feat: add transaction importer controller

---
+++ app/Controllers/TransactionImporterController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controllers;
+
+use App\Contracts\RequestValidatorFactoryInterface;
+use App\DataObjects\TransactionData;
+use App\RequestValidators\TransactionImportRequestValidator;
+use App\Services\CategoryService;
+use App\Services\TransactionImportService;
+use App\Services\TransactionService;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\UploadedFileInterface;
+
+class TransactionImporterController
+{
+    public function __construct(
+        private readonly RequestValidatorFactoryInterface $requestValidatorFactory,
+        private readonly TransactionImportService $transactionImportService
+    ) {
+    }
+
+    public function import(Request $request, Response $response): Response
+    {
+        /** @var UploadedFileInterface $file */
+        $file = $this->requestValidatorFactory->make(TransactionImportRequestValidator::class)->validate(
+            $request->getUploadedFiles()
+        )['importFile'];
+
+        $user     = $request->getAttribute('user');
+
+        $this->transactionImportService->importFromFile($file->getStream()->getMetadata('uri'), $user);
+
+        return $response;
+    }
+}
