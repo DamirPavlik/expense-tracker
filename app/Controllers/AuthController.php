64862aee3893418275c5058eb2edfd8b99587196
DamirPavlik <damir.pavlik@gmail.com>
2024-07-08 19:27:36 +0200

    feat: add 2FA to auth controller

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -7,13 +7,15 @@ namespace App\Controllers;
 use App\Contracts\AuthInterface;
 use App\Contracts\RequestValidatorFactoryInterface;
 use App\DataObjects\RegisterUserData;
+use App\Enum\AuthAttemptStatus;
 use App\Exception\ValidationException;
 use App\RequestValidators\RegisterUserRequestValidator;
+use App\RequestValidators\TwoFactorLoginRequestValidator;
 use App\RequestValidators\UserLoginRequestValidator;
+use App\ResponseFormatter;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
-use Valitron\Validator;
 
 class AuthController
 {
@@ -21,15 +23,16 @@ class AuthController
         private readonly Twig $twig,
         private readonly RequestValidatorFactoryInterface $requestValidatorFactory,
         private readonly AuthInterface $auth,
+        private readonly ResponseFormatter $responseFormatter
+    ) {
+    }
 
-    ) {}
-
-    public function loginView(Request $request, Response $response): Response
+    public function loginView(Response $response): Response
     {
         return $this->twig->render($response, 'auth/login.twig');
     }
 
-    public function registerView(Request $request, Response $response): Response
+    public function registerView(Response $response): Response
     {
         return $this->twig->render($response, 'auth/register.twig');
     }
@@ -44,24 +47,45 @@ class AuthController
             new RegisterUserData($data['name'], $data['email'], $data['password'])
         );
 
-        return $response->withHeader("Location", "/")->withStatus(302);
+        return $response->withHeader('Location', '/')->withStatus(302);
     }
 
-    public function logOut(Request $request, Response $response): Response
+    public function logIn(Request $request, Response $response): Response
+    {
+        $data = $this->requestValidatorFactory->make(UserLoginRequestValidator::class)->validate(
+            $request->getParsedBody()
+        );
+
+        $status = $this->auth->attemptLogin($data);
+
+        if ($status === AuthAttemptStatus::FAILED) {
+            throw new ValidationException(['password' => ['You have entered an invalid username or password']]);
+        }
+
+        if ($status === AuthAttemptStatus::TWO_FACTOR_AUTH) {
+            return $this->responseFormatter->asJson($response, ['two_factor' => true]);
+        }
+
+        return $this->responseFormatter->asJson($response, []);
+    }
+
+    public function logOut(Response $response): Response
     {
         $this->auth->logOut();
 
         return $response->withHeader('Location', '/')->withStatus(302);
     }
 
-    public function logIn(Request $request, Response $response): Response
+    public function twoFactorLogin(Request $request, Response $response): Response
     {
-        $data = $this->requestValidatorFactory->make(UserLoginRequestValidator::class)->validate($request->getParsedBody());
+        $data = $this->requestValidatorFactory->make(TwoFactorLoginRequestValidator::class)->validate(
+            $request->getParsedBody()
+        );
 
-        if (!$this->auth->attemptLogin($data)) {
-            throw new ValidationException(['password' => ['You have entered an invalid password or email']]);
+        if (!$this->auth->attemptTwoFactorLogin($data)) {
+            throw new ValidationException(['code' => ['Invalid Code']]);
         }
 
-        return $response->withHeader('Location', '/')->withStatus(302);
+        return $response;
     }
 }
